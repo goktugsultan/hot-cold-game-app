b98be501af2d68131f8f022543733f1a7202897d
Ali Atakay <38188630+aliatakay@users.noreply.github.com>
2020-01-04 01:25:24 +0300

    add guesslist

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './Progress.css' 
 import { Typography } from '@material-ui/core'
 
-const Progress = ({ attempt,guessList,surname}) => (
+const Progress = ({ attempt,guessList,surname }) => (
     <div>
-        <Typography style={{marginTop:'50px'}} variant="h3">Guess # {attempt}</Typography>
+        <Typography style={{marginTop:'50px'}} variant="h3">Guess {attempt}</Typography>
         <ul className="progressBar_history">
             {guessList}
         </ul>
